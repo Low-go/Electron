e75daf7d4c0805a4107bc0f42cbfd9c51cd40948
Low-go <lorranalvesgaldino98@gmail.com>
2025-09-30 14:10:55 -1000

    initial test app running

--- renderer.js
+++ renderer.js
@@ -1,1 +1,2 @@
-const information = document.getElementById('info');
+const information = document.getElementById('info')
+information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
