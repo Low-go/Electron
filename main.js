c2a5a39427005d1ce86e714d5d14c2d2a4ddd248
Low-go <lorranalvesgaldino98@gmail.com>
2025-09-30 14:01:40 -1000

    configre the main and rendere process, window and bridge between both

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-    })
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+        preload: path.join(__dirname, 'preload.js') // trying to undertsand this, I think it acts like a bridge between the main and runtime processes
+    }
+  })
 
-    win.loadFile('index.html')
+  win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-    createWindow()
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
 })
+
+
+// closes app if all windows are closed for windows and linux
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
