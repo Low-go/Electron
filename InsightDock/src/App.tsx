ff98358e4b2a8cd087e2ef3497216285053eb71c
Low-go <lorranalvesgaldino98@gmail.com>
2025-11-03 09:40:40 -1000

    simple login created

--- InsightDock/src/App.tsx
+++ InsightDock/src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/electron-vite.animate.svg'
-import './App.css'
 import { Routes, Route} from 'react-router-dom';
+import Login from './renderer/components/Auth/Login';
+
 
 function App() {
 
 
   return (
     <>
+      <Login/>
     </>
   )
 }
